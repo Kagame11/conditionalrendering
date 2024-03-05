88935f5a7efd3f3737c8d02f0f91d2a108b9c0df
intwari kagame fred <intwari.k.fred@gmail.com>
2024-03-04 16:10:02 -0800

    conditional rendering in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+ function Item ({name, isPacked}) {
+   let itemContent = name;
+   if (isPacked) {
+    itemContent = name + "✔";
+   }
+    return (<li className="item">
+    {itemContent}
+  </li>);
+  }
+   
+
+
+export default function PackingList(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Sally ride packing list</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit"></Item>
+        <Item isPacked={true} name="Helmet with a golden leaf"></Item>
+        <Item isPacked={false} name="Photo of Tam"></Item>
+      </ul>
+    </section>
   );
 }
-
-export default App;
